1e8d084f3a056642348ef2ee3723a3348cfae6f0
tdtx <tdtxle@gmail.com>
2024-11-20 03:21:32 -0600

    add catalogo chat

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,19 +1,43 @@
 class QuestionsController < ApplicationController
+  
+  
+
   def create
     # Using the global GEMINI_CLIENT constant initialized in the initializer
     contents = {
-      contents: {
-        role: 'user',
-        parts: {
-          text: question  # Use the user's question
+      contents: [
+        {
+          role: 'user',
+          parts: {
+            text: 'Actúa como un vendedor experto en nuestra tienda de ropa. Responde a las preguntas de los clientes de forma amigable y profesional, proporcionando información detallada sobre los productos del catálogo, incluyendo precios, disponibilidad y características. Si el cliente solicita una recomendación, sugiere productos que se adapten a sus necesidades. Evita respuestas que no estén relacionadas con la venta de productos.'+
+            '\nSolidus T-Shirt - $19.99 - 20 unidades'+
+            '\nSolidus Long Sleeve - $19.99 - 20 unidades'+
+            '\nSolidus Women\'s T-Shirt - $19.99 - 20 unidades'+
+            '\nSolidus Snapback Cap - $15.99 - 20 unidades'+
+            '\nSolidus Hoodie Zip - $29.99 - 20 unidades'+
+            '\nRuby Hoodie - $29.99 - 20 unidades'+
+            '\nRuby Hoodie Zip - $29.99 - 20 unidades'+
+            '\nRuby Polo - $26.99 - 20 unidades'+
+            '\nSolidus Mug - $9.99 - 20 unidades'+
+            '\nRuby Mug - $9.99 - 20 unidades'+
+            '\nSolidus Tote - $15.99 - 20 unidades'+
+            '\nRuby Tote - $15.99 - 20 unidades'
+          }
+        },
+          {
+          role: 'user',
+          parts: {
+            text: question # Use the user's question
+          }
         }
-      }
+      ]
     }
 
+   
     # Variable to accumulate the streamed response
     @answer = ""
 
-    # Define the block to handle streaming
+     # Define the block to handle streaming
     stream_proc = Proc.new do |part_text, _event, _parsed, _raw|
       @answer += part_text  # Append each part of the response to @answer
     end
@@ -26,7 +50,7 @@ class QuestionsController < ApplicationController
       format.turbo_stream  # Renders a turbo_stream response
       # format.html { redirect_to questions_path, notice: 'Answer was successfully generated.' }
       format.html { head :no_content }  # Asegurarse de que no haya redirección ni carga completa de la página
-    end
+    end 
 
   rescue => e
     @answer = "Error: #{e.message}"
