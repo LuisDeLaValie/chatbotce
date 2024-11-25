f697baae4faa37e8be36dcd287eef1a492e5fd12
tdtx <tdtxle@gmail.com>
2024-11-25 03:02:10 -0600

    agregar avatar y voz

--- config/initializers/gemini_ai.rb
+++ config/initializers/gemini_ai.rb
@@ -1,4 +1,5 @@
 require 'gemini-ai'
 
 # Configure the Gemini AI client with your API key
-GEMINI_CLIENT = GeminiAi::Client.new(api_key: 'AIzaSyAZsLYYIC14qsCu32RWg9KRpuN79jznXpo')
+# GEMINI_CLIENT = GeminiAi::Client.new(api_key: 'AIzaSyAZsLYYIC14qsCu32RWg9KRpuN79jznXpo')
+GEMINI_CLIENT = GeminiAi::Client.new(api_key: 'AIzaSyCb8y9FHmDNxE0fPTtHltytgDvg9X5d7jg')
